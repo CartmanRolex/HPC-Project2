42e8d3a56201c4d75c37d3888d27ab1721aeb7e1
Gal <gal.pascual@epfl.ch>
2024-11-19 17:22:49 +0100

    correct dataset function generator

--- Dataset/dataset1.py
+++ Dataset/dataset1.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-getPolMatrix(n=1e3,R=10,p=1):
+def getPolMatrix(n=1e3,R=10,p=1):
     diag_elements = np.concatenate((np.ones(R), np.power(np.arange(2, n - R + 2, dtype=float), -p)))
     A = np.diag(diag_elements)
     return A
     
-getExpMatrix(n=1e3,R=10,q=0.25):
+def getExpMatrix(n=1e3,R=10,q=0.25):
     diag_elements = np.concatenate((np.ones(R), np.power(10, -q * np.arange(1, n - R + 1))))    
     A = np.diag(diag_elements)
     return A
